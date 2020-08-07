ae6e23591d04c47a1aa0e9fd91e5dcdd2687ff4e
tumaranja <tumaranja@gmail.com>
2020-08-07 21:49:21 +0300

    hero section complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,32 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="Hero">
+    <nav className="NavWrapper">
+      <ul className="Nav">
+        <li><a href=""><img className="NavItem DesignCodeLogo" src="../images/DesignCode-Logo-White.svg"/></a></li>
+        <li><a className="NavItem">Courses</a></li>
+        <li><a className="NavItem">Downloads</a></li>
+        <li><a className="NavItem">Workshops</a></li>
+        <li><a className="NavItem">Sign in</a></li>
+        <li><a className="NavItem SignUp">Try for free</a></li>
+      </ul>
+    </nav>
+      <div className="HeroGroup">
+        <h1 className="Title">Learn to design and code <span>React apps</span></h1>
+         <p className="Intro">Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+         <a className="HeroLink">Watch the Video</a>
+         <ul className="AppLogos">
+          <li className="AppLogo"><img src="../images/logo-sketch.png"></img></li>
+          <li className="AppLogo"><img src="../images/logo-figma.png"></img></li>
+          <li className="AppLogo"><img src="../images/logo-invision.png"></img></li>
+          <li className="AppLogo"><img src="../images/logo-framer.png"></img></li>
+          <li className="AppLogo"><img src="../images/logo-react.png"></img></li>
+          <li className="AppLogo"><img src="../images/logo-xcode.png"></img></li>
+          <li className="AppLogo"><img src="../images/logo-swift.png"></img></li>
+         </ul>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
